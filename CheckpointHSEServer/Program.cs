efbbae024c15f4c544c1d15da340cdf39d7f4f51
Полина Полыгалова <pnpolygalova@edu.hse.ru>
2021-02-17 19:43:44 +0300

    1

---
+++ CheckpointHSEServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckpointHSEServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
