707063ccb486a35d939c0ef2b31ae4b5809c3393
Екатерина Семчук <easemchuk@edu.hse.ru>
2021-06-24 09:06:24 +0300

    Добавлено CheckpointHSEApp

---
+++ CheckpointHSEApp/AdditionalInfoWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+namespace CheckpointHSEApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AdditionalInfoWindow.xaml
+    /// </summary>
+    public partial class AdditionalInfoWindow : Window
+    {
+        private System.Windows.Forms.PictureBox PersonPictureBox = new System.Windows.Forms.PictureBox();
+
+        public AdditionalInfoWindow(System.Drawing.Image image, string info)
+        {
+            InitializeComponent();
+            PersonHost.Child = PersonPictureBox;
+            PersonPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            PersonPictureBox.Image = image;
+            InfoLabel.Content = info;
+        }
+        public AdditionalInfoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GateOpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            new OpenGateWindow().ShowDialog();
+        }
+    }
+}
---
+++ CheckpointHSEApp/OpenGateWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CheckpointHSEApp
+{
+    /// <summary>
+    /// Логика взаимодействия для OpenGateWindow.xaml
+    /// </summary>
+    public partial class OpenGateWindow : Window
+    {
+        public OpenGateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
